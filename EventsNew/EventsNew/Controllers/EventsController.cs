f38a90f2430fcffe2fec46990d965d2321e8f9ff
shira-s1 <shirasportes@gmail.com>
2024-10-30 16:37:44 +0200

    my commit message

---
+++ EventsNew/EventsNew/Controllers/EventsController.cs
@@ -0,0 +1,50 @@
+﻿using EventsNew.logic;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EventsNew.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventsController : ControllerBase
+    {
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public Events[] Get()
+        {
+            Events[] events = { new Events() { start = new DateTime(), title = "תור לרופא שיניים" },
+             new Events() { start = new DateTime().AddDays(2), title = "לא לשכוח ללכת לקחת את השקית" }};
+            return events;
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public int Get(int id)
+        {
+            return 2;
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
